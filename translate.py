362cf0a731adbccb77d19d30bff21d7c43784da0
Shelwyn Corte <shelwyn.corte@outlook.com>
2022-04-11 20:38:46 +0530

    Update translate.py

--- translate.py
+++ translate.py
@@ -8,16 +8,20 @@ r = sr.Recognizer()
 with sr.Microphone() as source:
     print("Speak Now:")
     audio = r.listen(source)
-try:
-    #List of Language names: https://meta.wikimedia.org/wiki/Template:List_of_language_names_ordered_by_code
-    print("Recognized as: ", r.recognize_google(audio))
-    language = "hi" #change language here
-    translations=translator.translate(r.recognize_google(audio), dest=language) #
-    print(translations.origin, ' -> ', translations.text)
-    myobj = gTTS(text=translations.text, lang=language)
-    myobj.save("speech.mp3")
-    playsound("speech.mp3")
-except sr.UnknownValueError:
-    print("Audio Error")
-except sr.RequestError as e:
-    print("Cloud Error; {0}".format(e))
+
+trans=['hi','mr','ml','ta'] #List of Language names: https://meta.wikimedia.org/wiki/Template:List_of_language_names_ordered_by_code
+for tr in trans:
+    print(tr)
+    try:
+        print("Recognized as: ", r.recognize_google(audio))
+        language = tr #change language here
+        translations=translator.translate(r.recognize_google(audio), dest=language) #
+        print(translations.origin, ' -> ', translations.text)
+        myobj = gTTS(text=translations.text, lang=language)
+        myobj.save(tr + ".mp3")
+        playsound(tr + ".mp3")
+    except sr.UnknownValueError:
+        print("Audio Error")
+    except sr.RequestError as e:
+        print("Cloud Error; {0}".format(e))
+
